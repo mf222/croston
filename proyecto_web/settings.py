b4a123dab0e0e58b2cd3b5a7c1eb3c4d5f75ee71
mf222 <mfsepulveda@uc.cl>
2016-11-16 00:38:17 +0000

    Working in deploy

--- proyecto_web/settings.py
+++ proyecto_web/settings.py
@@ -101,3 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
+
+FILE_UPLOAD_HANDLERS = (
+    'django.core.files.uploadhandler.TemporaryFileUploadHandler',
+)
